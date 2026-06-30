0c10facf2077b83647fed2f08d440453267bac71
Gucc1 <bao992001pth@gmail.com>
2026-06-30 21:07:55 +0700

    changing damage on electric efect

--- Assets/ElectricEffect.cs
+++ Assets/ElectricEffect.cs
@@ -17,7 +17,7 @@ public class ElectricEffect : MonoBehaviour
         _results = new Collider2D[20];
     }
 
-    public void DamageTargetInRadius()
+    public void DamageTargetInRadius(float damage)
     {
         int dectects = Physics2D.OverlapCircleNonAlloc(transform.position,_damageRadius,_results);
         for(int i = 0 ; i < dectects; i++)
@@ -25,8 +25,7 @@ public class ElectricEffect : MonoBehaviour
             if(_results[i].TryGetComponent<IHit>(out var hit))
             {
                 // DebugCustom.Log("Electric passive hit enemy "+ ServiceLocator.Get<Player>().entityStat.GetElementalDamageDisplay()*1.1f);
-                hit?.TakeDamage(0,ServiceLocator.Get<Player>().entityStat.GetElementalDamageValue(out _)*1.1f,ElementType.Electric,transform);
-
+                hit?.TakeDamage(0,damage*1.1f,ElementType.Electric,transform);
             }
         }
     }
